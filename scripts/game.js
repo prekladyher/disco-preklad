8883cfb769c2b78abbd2a4b081d9550014cdbec0
Pavel Horal <pavel.horal@orchitech.cz>
2022-02-19 17:56:48 +0100

    Add depth inspection option to game extraction script.

--- scripts/game.js
+++ scripts/game.js
@@ -20,13 +20,18 @@ program.command("read")
   .argument("<type>", "asset data type ('LanguageSourceAsset' or 'DialogueDatabase'")
   .argument("<file>", "script data file")
   .option("-p, --path <path>", "JSON path transform (e.g. '$.mSource.mTerms[*].Term')")
+  .option("-d, --depth <depth>", "inspection path depth")
   .action(async (type, file, options) => {
     const decoded = decode(type, await fs.readFile(file));
+    const inspect = { ...INSPECT_OPTS };
+    if (options.depth !== undefined) {
+      inspect.depth = parseInt(options.depth, 10);
+    }
     if (options.path) {
       const { JSONPath } = await import("jsonpath-plus");
-      console.dir(JSONPath({ path: options.path, json: decoded }), INSPECT_OPTS);
+      console.dir(JSONPath({ path: options.path, json: decoded }), inspect);
     } else {
-      console.dir(decoded, INSPECT_OPTS);
+      console.dir(decoded, inspect);
     }
   });
 
