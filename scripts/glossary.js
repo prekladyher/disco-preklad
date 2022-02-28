3bea7663dbe7056113127748f60d5898f525e040
Fluik <flu.jane@centrum.cz>
2022-02-28 08:37:55 +0100

    generate glossary

--- scripts/glossary.js
+++ scripts/glossary.js
@@ -1,7 +1,5 @@
 import fs from "fs";
-import { program } from "commander";
 import { GoogleSpreadsheet } from 'google-spreadsheet';
-import  * as auth  from './auth.js'
 
 const doc = new GoogleSpreadsheet('1GMYp3UxK3nb5LX6QzQO8vLpDeY5ikrSnFZxIkJzE_KA');
 const glossaryFile = "./terms.tbx";
@@ -9,40 +7,42 @@ const header = "<?xml version='1.0' encoding='UTF-8'?><!DOCTYPE martif PUBLIC 'I
 const footer = "</body></text></martif>";
 
 
-program
-  .description("Export gloosary for Lokalize")
-  .action(async () => {
+async function generateGlossary() {
     await doc.useServiceAccountAuth({
-      client_email: auth.GGclient_email,
-      private_key: auth.GGprivate_key,
+      client_email: process.env.GGCLIENT_EMAIL,
+      private_key: process.env.GGPRIVATE_KEY.replace(/\\n/gm, '\n')
     });
 
     await doc.loadInfo(); // loads document properties and worksheets
-    console.log(doc.title);
+    console.log('DOCS: '+doc.title);
     const sheet = doc.sheetsByIndex[0]; // use doc.sheetsByIndex[0] or doc.sheetsById[id] or doc.sheetsByTitle[title]
     const rows = await sheet.getRows();
     var lastRow = sheet.headerValues[7]-1;
     fs.writeFileSync(glossaryFile, header);
 
     for(var i = 0; i < lastRow;i++){
+      let poznamka = '';
+      let vyraz = '';
+      let preklad = '';
       let schvaleno = '';
       let velikost = '';
-      let poznamka = rows[i].Poznámka.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-      let vyraz = rows[i].Výraz.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-      let preklad = rows[i].Překlad.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
 
+      if (typeof rows[i].Poznámka !== "undefined") { poznamka = rows[i].Poznámka.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;"); }
+      if (typeof rows[i].Výraz !== "undefined") { vyraz = rows[i].Výraz.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;"); }
+      if (typeof rows[i].Překlad !== "undefined") { preklad = rows[i].Překlad.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;"); }
       if (rows[i].OK == "x") { schvaleno = "[OK] " }
       if (rows[i].aA == "x") { velikost = "[aA] " }
+
       let glossaryRow = '<termEntry id="PHS-'+[i]+'"><descrip type="definition">'+schvaleno+velikost+poznamka+'</descrip><langSet xml:lang="en-US"><tig><term>'+vyraz+'</term></tig></langSet><langSet xml:lang="cs-CZ"><tig><term>'+preklad+'</term></tig></langSet></termEntry>';
       fs.appendFileSync(glossaryFile, glossaryRow);
     }
 
     fs.appendFileSync(glossaryFile, footer);
 
-  })
+}
 
+generateGlossary();
 
-  program.parseAsync();
 
 
 
