1f8c861d9808da61c949fc1033e6efac0d4a3263
Fluik <flu.jane@centrum.cz>
2022-03-02 10:35:35 +0100

    prettifyXml

--- scripts/glossary.js
+++ scripts/glossary.js
@@ -1,6 +1,34 @@
 import fs from "fs";
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 
+function prettifyXml(xmlInput) {
+  const indentString = '  ';
+  const newlineOption = "\n";
+  let formatted = ''
+  const regex = /(>)(<)(\/*)/g
+  const xml = xmlInput.replace(regex, `$1${newlineOption}$2$3`)
+  let pad = 0
+  xml.split(/\r?\n/).forEach(l => {
+    const line = l.trim()
+    let indent = 0
+    if (line.match(/.+<\/\w[^>]*>$/)) {
+      indent = 0
+    } else if (line.match(/^<\/\w/)) {
+      if (pad !== 0) {
+        pad -= 1
+      }      
+    } else if (line.match(/^<\w([^>]*[^\/])?>.*$/)) {
+      indent = 1       
+    } else {
+      indent = 0
+    }
+    const padding = Array(pad + 1).join(indentString)
+    formatted += padding + line + newlineOption
+    pad += indent
+  })
+  return formatted.trim()
+}
+
 const doc = new GoogleSpreadsheet('1GMYp3UxK3nb5LX6QzQO8vLpDeY5ikrSnFZxIkJzE_KA');
 const glossaryFile = "./terms.tbx";
 const header = "<?xml version='1.0' encoding='UTF-8'?><!DOCTYPE martif PUBLIC 'ISO 12200:1999A//DTD MARTIF core (DXFcdV04)//EN' 'TBXcdv04.dtd'><martif type=\"TBX\" xml:lang=\"en_US\"><text><body>";
@@ -18,7 +46,7 @@ async function generateGlossary() {
     const sheet = doc.sheetsByIndex[0]; // use doc.sheetsByIndex[0] or doc.sheetsById[id] or doc.sheetsByTitle[title]
     const rows = await sheet.getRows();
     var lastRow = sheet.headerValues[7]-1;
-    fs.writeFileSync(glossaryFile, header);
+    let glossaryData = header;
 
     for(var i = 0; i < lastRow;i++){
       let poznamka = '';
@@ -33,11 +61,11 @@ async function generateGlossary() {
       if (rows[i].OK == "x") { schvaleno = "[OK] " }
       if (rows[i].aA == "x") { velikost = "[aA] " }
 
-      let glossaryRow = '<termEntry id="PHS-'+[i]+'"><descrip type="definition">'+schvaleno+velikost+poznamka+'</descrip><langSet xml:lang="en-US"><tig><term>'+vyraz+'</term></tig></langSet><langSet xml:lang="cs-CZ"><tig><term>'+preklad+'</term></tig></langSet></termEntry>';
-      fs.appendFileSync(glossaryFile, glossaryRow);
+      glossaryData += '<termEntry id="PHS-'+[i]+'"><descrip type="definition">'+schvaleno+velikost+poznamka+'</descrip><langSet xml:lang="en-US"><tig><term>'+vyraz+'</term></tig></langSet><langSet xml:lang="cs-CZ"><tig><term>'+preklad+'</term></tig></langSet></termEntry>';
     }
-
-    fs.appendFileSync(glossaryFile, footer);
+    
+    glossaryData += footer;
+    fs.writeFileSync(glossaryFile, prettifyXml(glossaryData));
 
 }
 
