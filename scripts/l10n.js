559883585030b32a7e7de4e04a226dd1fad4dfcd
Pavel Horal <pavel.horal@orchitech.cz>
2022-03-12 21:40:04 +0100

    Compute statistics on push.

--- scripts/l10n.js
+++ scripts/l10n.js
@@ -1,7 +1,9 @@
 import { program } from "commander";
+import { copySource } from "./l10n/copy.js";
 import { calcStats, extractAsset } from "./l10n/main.js";
 import { mergeL10n } from "./l10n/merge.js";
-import { copySource } from "./l10n/copy.js";
+import chalk from "chalk";
+import { inspect } from "util";
 
 program
   .name("l10n")
@@ -20,8 +22,27 @@ program.command("extract")
 program.command("stats")
   .description("Provide simple translation statistics")
   .argument("<base>", "base path (file or directory)")
-  .action(async (base) => {
-    calcStats(base).forEach(stats => console.log(JSON.stringify(stats)));
+  .option("-f, --file", "print individual file statistics")
+  .option("-s, --summary", "print overall statistics summary")
+  .action(async (base, options) => {
+    const stats = calcStats(base);
+    if (!options.summary || options.file) {
+      stats.forEach(file => {
+        console.log(
+          chalk.green(`${file[0]}:\t`),
+          inspect(file[1], { compact: true, breakLength: Number.MAX_SAFE_INTEGER, colors: true }));
+      });
+    }
+    if (options.summary) {
+      const summary = stats.map(it => it[1]).reduce((acc, cur) => {
+        return Object.fromEntries(Object.keys(acc).map(key => [key, cur[key] + acc[key]]))
+      });
+      console.log(
+        chalk.cyan(`Summary: `),
+        inspect(summary, { compact: true, breakLength: Number.MAX_SAFE_INTEGER, colors: true }));
+      const completion = summary.targetCount / summary.sourceCount;
+      console.log(chalk.cyan("Completion: "), chalk.red(completion.toFixed(2) + " %"));
+    }
   });
 
 program.command("merge")
