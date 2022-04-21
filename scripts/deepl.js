64ed8004df29e503d350b38611c8905de1da0029
Fluik <flu.jane@centrum.cz>
2022-04-21 10:51:03 +0200

    deepl script rewrite

--- scripts/deepl.js
+++ scripts/deepl.js
@@ -1,26 +1,48 @@
 import fs from "fs";
 import { program } from "commander";
-import translate from "deepl";
+import deepl from "deepl-node";
 import * as auth from './auth.js'
 import { decodeEntries } from "./text/main.js";
 import { encodeTextFile,writeTextFile } from "./l10n/utils.js"; 
 
 const sourceDirBase = "./source/l10n/cs/Dialogues/";
+const translator = new deepl.Translator(auth.deeplAuth);
+const usage = await translator.getUsage();
+let mark = "DEEPL: ";
 
-function deeplTranslate(string, callback){
-  translate({
-    free_api: true,
-    text: string,
-    source_lang: 'EN',
-    target_lang: 'CS',
-    auth_key: auth.deeplAuth,
-  }).then(result => {          
-    callback(result.data.translations[0].text);
+function translate(sourceFile, nomark){
+  console.log('File: '+sourceFile);
+  let data = decodeEntries(fs.readFileSync(sourceFile).toString()).filter(entry => !!entry.msgid); 
+
+  data.forEach(el => {
+    translator  
+    .translateText(el.msgid, null, 'cs')
+    .then((result) => {
+      if (nomark) mark = "";
+      el.msgstr = mark+result.text.toString().replace(/[^ěščřžýáíéóúůďťňĎŇŤŠČŘŽÝÁÍÉÚŮĚÓa-zA-Z]*$/gi, '');
+      writeTextFile(sourceFile.replace('/l10n/' ,'/deepl/'), encodeTextFile('cs', data));  
+      console.log('Translating... '+el.msgctxt);  
+    })
+    .catch((error) => {
+        console.error(error);
+    });
   })
-  .catch(error => {
-      console.error(error)
-  })
-}
+
+}  
+
+
+
+program
+  .command('stats')
+  .action((options) => {
+    if (usage.anyLimitReached()) {
+        console.log('Translation limit exceeded.');
+    }
+    if (usage.character) {
+        console.log(`Characters: ${usage.character.limit-usage.character.count}`);
+    }
+  });  
+
 
 
 program
@@ -29,52 +51,19 @@ program
   .option('-nm, --nomark', 'No deepl marking')
   .action((options) => {
 
-    let mark = "DEEPL: ";
-
     if (options.file){
-      console.log('File: '+sourceDirBase+options.file);
       let sourceFile = sourceDirBase+options.file;
-      let data = decodeEntries(fs.readFileSync(sourceFile).toString()).filter(entry => !!entry.msgid); 
-
-      data.forEach(el => {
-        deeplTranslate(el.msgid, function(translation) {    
-          if (options.nomark) mark = "";
-          el.msgstr = mark+translation.toString().replace(/[^ěščřžýáíéóúůďťňĎŇŤŠČŘŽÝÁÍÉÚŮĚÓa-zA-Z]*$/gi, '');
-          writeTextFile(sourceFile.replace('/l10n/' ,'/deepl/'), encodeTextFile('cs', data));  
-          console.log('Translating... '+el.msgctxt);   
-        });
-      })
-      
+      translate(sourceFile, options.nomark);
     } else if (options.dir){
-      console.log('Directory: '+sourceDirBase+options.dir);
       let sourceDir = sourceDirBase+options.dir+'/';
-
       fs.readdirSync(sourceDir).forEach(file => {
-        console.log('File to translate: '+file);
         let sourceFile = sourceDir+file;
-        let data = decodeEntries(fs.readFileSync(sourceFile).toString()).filter(entry => !!entry.msgid); 
-
-        data.forEach(el => {
-          deeplTranslate(el.msgid, function(translation) {  
-            if (options.nomark) mark = "";
-            el.msgstr = mark+translation.toString().replace(/[^ěščřžýáíéóúůďťňĎŇŤŠČŘŽÝÁÍÉÚŮĚÓa-zA-Z]*$/gi, '');
-            writeTextFile(sourceFile.replace('/l10n/' ,'/deepl/'), encodeTextFile('cs', data));  
-            console.log('Translating... '+el.msgctxt);          
-          });
-        })
-
-      })  
-
-
+        translate(sourceFile, options.nomark);
+      });  
     } else {
       console.log('You must specify file or directory.');
     } 
-
-
-
-
-}); 
-
-
+    
+  }); 
 
 program.parse(process.argv);
